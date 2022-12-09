a06031bbd69beaf8e695bf48834615020bc0ceae
prevail ugah <prevailbugah@gmail.com>
2022-12-09 14:55:59 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main-to print the specified strinf
+ *
+ * Description: A c program to print with put function
+ *
+ * Return : Always 0 to exit (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
