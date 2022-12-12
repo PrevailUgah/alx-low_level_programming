236d1de764c6694bca9ad1e988186421d5bbe6ef
prevail ugah <prevailbugah@gmail.com>
2022-12-12 06:56:22 -0800

    task6 erro fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,12 +4,17 @@
  * return - Always 0 (success)
  */
 int main(void)
-{
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 
-		printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+		printf("Size of an int: %lu byte(s)\n", sizeof(int));
 
-			printf("Size of a float: %lu byte(s)\n", sizeof(float));
+			printf("Size of an double: %lu byte(s)\n", sizeof(double));
 
-				return (0);
+				printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+
+					printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+
+						printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+							return (0);
 }
