eeebeb6a625e5e536cdd6a4363bb713eb1eefc2b
prevail ugah <prevailbugah@gmail.com>
2022-12-12 05:48:38 -0800

    task6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry level
- * Return - Always 0
+ * main - Entry point
+ * return: Always 0 (success)
  */
 int main(void)
-
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * return - Always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+
+		printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+
+			printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+				return (0);
+}
