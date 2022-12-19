5bc1112b9c2dc4e311ba97e032707414cba102d1
prevail ugah <prevailbugah@gmail.com>
2022-12-19 13:12:43 -0800

    solved task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_rev -> printing a string in reverse
+* @s: the string to be printed in rev
+* Owned by Prevail Ugah
+*/
+
+void print_rev(char *s)
+{
+	int i, n;
+
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
