661fa558d0a688abcd415c8b2df627c9ec9b87ff
prevail ugah <prevailbugah@gmail.com>
2022-12-19 13:10:18 -0800

    solved task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: the string
+ *
+ * Owned by Prevail Ugah
+ * Return: the length of the string
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
